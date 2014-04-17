b6dea3c73dd13a9c365a20a61ba80769593d28f9
alinoil <noil@live.nl>
2014-04-17 14:27:07 +0200

    Login.php
    dom voor inloggen

--- templates/inloggen.php
+++ templates/inloggen.php
@@ -1,9 +1,29 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		
+			<title>Inloggen</title>
 	</head>
 	<body>
-		<h1>Test</h1>
+		<h1>inloggen</h1>
+		<form method="post" action="" >
+		<table border="1">
+		<tr>
+			<td>gebruikersnaam</td>
+			<td><input type="text" name="username" /></td>			
+		</tr>
+		<tr>
+			<td>wachtwoord</td>
+			<td><input type="password" /></td>
+			<input type="hidden" name="contrle" value="test" />
+		</tr>
+		<tr>
+			<td colspan="2"><input type="button" value="inloggen" name="login"></td>
+		</tr>
+		<tr>
+			<td><a href="">wachtwoord vergeten</a></td>
+		</tr>
+		</table>
+		</form>
+		
 	</body>
 </html>
