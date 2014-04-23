02ae957d61018e55c6ba4d6d7e80456236b83d5a
Jos <joslast@hotmail.nl>
2014-04-23 15:53:34 +0200

    Create Databaseclass
    added Database.php

---
+++ classes/Database.php
@@ -0,0 +1,34 @@
+<?php
+	class Database {
+		private $db_object;
+
+		public function connect(){
+			try{
+				$db = new PDO('mysql:host=localhost;dbname=pw7', 'root', '');
+				$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
+			}
+				catch (PDOException $e){
+				echo $e->getMessage();
+				exit;
+			}
+
+			$this->db_object = $db;
+		}
+
+		public function __get($property) {
+    		if (property_exists($this, $property)) {
+      			return $this->$property;
+    		}
+  		}
+
+  		public function __set($property, $value) {
+    		if (property_exists($this, $property)) {
+      			$this->$property = $value;
+    		}
+  		}
+
+
+	}
+
+
+?>
--- config/config.php
+++ config/config.php
@@ -1,19 +1,19 @@
 <?php
-	//database connectie
-	try{
-		$db = new PDO('mysql:host=localhost;dbname=pw7', 'root', '');
-		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
-	}
-	catch (PDOException $e){
-		echo $e->getMessage();
-	exit;
-	}
-
+	
 	//globalrequestpath
 	$globalPath = $_SERVER['REQUEST_URI'];
 
 	//include classes
 	include'../PW7/classes/Message.php';
 	include'../PW7/classes/User.php';
+	include'../PW7/classes/Database.php';
+
+	//globals
+	global $database;
+
+	//database connection
+	$database = new Database();
+	$database->connect();
+
 
 ?>
