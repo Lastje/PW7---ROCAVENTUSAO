b3236397243ad103506a1f1e8d8d202eabb32fc5
alinoil <noil@live.nl>
2014-04-24 14:45:05 +0200

    Index gefixed
    gefixed

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
 	} elseif ($globalPath == '/PW7/inloggen/') {
 		include'../PW7/actions/doLogin.php';
 	} elseif ($globalPath == '/PW7/wachtwoordvergeten/') {
-		include'../PW7/actions/passwordForgot.php';
+		include'../PW7/templates/passwordForgot.php';
 	} elseif ($globalPath == '/PW7/registreren/') {
-		include'../PW7/actions/registratie.php';
+		include'../PW7/templates/registratie.php';
 	} else {
 		include'../PW7/templates/inloggen.php';
 	}
