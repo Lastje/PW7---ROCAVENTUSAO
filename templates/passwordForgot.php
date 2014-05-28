9e23e100845dab52bc29cef79ad589684c1fdfa9
alinoil <noil@live.nl>
2014-05-28 14:14:18 +0200

    little bit of opmaak

--- templates/passwordForgot.php
+++ templates/passwordForgot.php
@@ -10,6 +10,10 @@
 	</div>
 	
 	<div class="innerContent">
+	<div id="option_menu">
+            <div class="option_item"><i class="fa fa-pencil"></i> <a href="../../PW7/inloggen/">Terug</a> </div>
+        </div>
+		
 	<form method="post" action="../../PW7/DopasswordForgot/">
 		<div> voer uw gebruikersnaam in om een nieuw wachtwoord aa te vragen. </div>
 		<div><input type="text" name="username" /> </div>
